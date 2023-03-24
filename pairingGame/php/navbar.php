ecb9a0300442d5bd071207c8f7405bb02c5c2577
Jacob <jw.braidley@gmail.com>
2023-03-24 12:45:09 +0000

    align items in navabr

--- pairingGame/php/navbar.php
+++ pairingGame/php/navbar.php
@@ -1,21 +1,29 @@
 <body>
     <nav class="navbar navbar-expand-lg">
-        <ul class="navbar-nav">
-            <li class="nav-item" name="home">
-                <a class="nav-link" href="index.php">Home</a>
-            </li>
-            <li class="nav-item" name="memory">
-                <a class="nav-link" href="pairs.php">Play Pairs</a>
-            </li>
-            <?php if($registered) { ?>
-                <li class="nav-item" name="leaderboard">
-                    <a class="nav-link" href="leaderboard.php" name="leaderboard">Leaderboard</a>
-                </li>
-            <?php } else { ?>
-                <li class="nav-item" name="leaderboard">
-                    <a class="nav-link" href="registration.php" name="register">Register</a>
-            </li>
-            <?php } ?>
-        </ul>
+        <div class="container-fluid">
+            <div class="justify-content-start">
+                <ul class="navbar-nav">
+                    <li class="nav-item me-auto" name="home">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="justify-content-end">
+                <ul class="navbar-nav">
+                    <li class="nav-item" name="memory">
+                        <a class="nav-link" href="pairs.php">Play Pairs</a>
+                    </li>
+                    <?php if($registered) { ?>
+                        <li class="nav-item" name="leaderboard">
+                            <a class="nav-link" href="leaderboard.php" name="leaderboard">Leaderboard</a>
+                        </li>
+                    <?php } else { ?>
+                        <li class="nav-item" name="leaderboard">
+                            <a class="nav-link" href="registration.php" name="register">Register</a>
+                        </li>
+                    <?php } ?>
+                </ul>
+            </div>
+        </div>
     </nav>
 </body>
