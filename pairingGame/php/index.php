4a1a3f7979bca640b69eddafd82a062b017b88f9
Jacob <jw.braidley@gmail.com>
2023-03-24 15:24:34 +0000

    Update index.php

--- pairingGame/php/index.php
+++ pairingGame/php/index.php
@@ -7,7 +7,6 @@
         <title>INDEX</title>
     </head>
     <body>
-        <?php $registered = true; ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
         <?php include("navbar.php"); ?>
         <div id="main">
