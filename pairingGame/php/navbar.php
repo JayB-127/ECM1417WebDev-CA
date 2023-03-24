14bcd93d0b60ea556bd436769a0249e622a3b8ad
Jacob <jw.braidley@gmail.com>
2023-03-24 11:53:06 +0000

    navbar content

--- pairingGame/php/navbar.php
+++ pairingGame/php/navbar.php
@@ -1,7 +1,17 @@
 <body>
-    <ul id="navbar">
-        <li>Home</li>
-        <li>Play Pairs</li>
-        <li>Leaderboard/Register</li>
-    </ul>
+    <nav class="navbar navbar-expand-lg">
+        <ul class="navbar-nav">
+            <li class="nav-item" name="home">
+                <a href="index.php" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item" name="memory">
+                <a href="pairs.php" class="nav-link">Play Pairs</a>
+            </li>
+            <?php if($registered) { ?>
+                <a class="nav-link" href="leaderboard.php" name="leaderboard">Leaderboard</a>
+            <?php } else { ?>
+                <a class="nav-link" href="registration.php" name="register">Register</a>
+            <?php } ?>
+        </ul>
+    </nav>
 </body>
