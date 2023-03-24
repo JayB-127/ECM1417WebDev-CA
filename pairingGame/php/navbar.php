884b747fb82dcb45249e863630daaf84aa640189
Jacob <jw.braidley@gmail.com>
2023-03-24 11:58:57 +0000

    php if statement syntax

--- pairingGame/php/navbar.php
+++ pairingGame/php/navbar.php
@@ -2,15 +2,19 @@
     <nav class="navbar navbar-expand-lg">
         <ul class="navbar-nav">
             <li class="nav-item" name="home">
-                <a href="index.php" class="nav-link">Home</a>
+                <a class="nav-link" href="index.php">Home</a>
             </li>
             <li class="nav-item" name="memory">
-                <a href="pairs.php" class="nav-link">Play Pairs</a>
+                <a class="nav-link" href="pairs.php">Play Pairs</a>
             </li>
             <?php if($registered) { ?>
-                <a class="nav-link" href="leaderboard.php" name="leaderboard">Leaderboard</a>
+                <li class="nav-item" name="leaderboard">
+                    <a class="nav-link" href="leaderboard.php" name="leaderboard">Leaderboard</a>
+                </li>
             <?php } else { ?>
-                <a class="nav-link" href="registration.php" name="register">Register</a>
+                <li class="nav-item" name="leaderboard">
+                    <a class="nav-link" href="registration.php" name="register">Register</a>
+            </li>
             <?php } ?>
         </ul>
     </nav>
