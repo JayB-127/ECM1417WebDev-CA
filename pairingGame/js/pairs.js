a554843703a66300fc288f7b096fa620a793cbd3
Jacob <jw.braidley@gmail.com>
2023-04-05 20:55:20 +0100

    detects when game won

--- pairingGame/js/pairs.js
+++ pairingGame/js/pairs.js
@@ -1,4 +1,5 @@
 var flipped = 0;
+var totalFlipped = 0;
 var flippedCards = [];
 
 function flip(elem) {
@@ -16,23 +17,17 @@ function flip(elem) {
         }
         elem.style.transform = "rotateY(-180deg)";
 
-        var skin = [];
-        var eyes = [];
-        var mouth = [];
-
-        //for the last two cards that were flipped
-        for (var i = 0; i < flippedCards.length; i++) {
-            let card = document.getElementById(flippedCards[i])
-            skin.push(card.children[1].children[0].children[0].src.split("/").pop()); //takes string after last "/" in src
-            eyes.push(card.children[1].children[0].children[1].src.split("/").pop());
-            mouth.push(card.children[1].children[0].children[2].src.split("/").pop());
-        }
-
-        if (skin[0] === skin[1] && eyes[0] === eyes[1] && mouth[0] === mouth[1]) {
+        //compare avatar features of both cards
+        var comparison = compareAvatars(flippedCards);
+        if (comparison == true) {
             for (var i = 0; i < cards.length; i++) {
                 if (cards[i].style.transform !== "rotateY(-180deg)") {
                     cards[i].style.removeProperty("pointer-events");
                 }
+            }
+            totalFlipped += 2;
+            if (totalFlipped == 10) {
+                //game won
             }
         } else {
             setTimeout(function() {
@@ -41,8 +36,29 @@ function flip(elem) {
                     cards[i].style.removeProperty("pointer-events"); //make all cards clickable
                 }
             }, 500);
+            totalFlipped = 0;
         }
+
         flipped = 0;
         flippedCards = [];
     }
+}
+
+function compareAvatars(flippedCards) {
+    var skin = [];
+    var eyes = [];
+    var mouth = [];
+
+    //for the last two cards that were flipped
+    for (var i = 0; i < flippedCards.length; i++) {
+        let card = document.getElementById(flippedCards[i])
+        skin.push(card.children[1].children[0].children[0].src.split("/").pop()); //takes string after last "/" in src
+        eyes.push(card.children[1].children[0].children[1].src.split("/").pop());
+        mouth.push(card.children[1].children[0].children[2].src.split("/").pop());
+    }
+
+    if (skin[0] == skin[1] && eyes[0] == eyes[1] && mouth[0] == mouth[1]) {
+        return true;
+    }
+    return false;
 }
