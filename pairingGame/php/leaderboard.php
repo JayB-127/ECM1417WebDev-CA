ed084a983abc601b04a1b7b51e6a8abdb87b873b
Jacob <jw.braidley@gmail.com>
2023-04-13 16:16:07 +0100

    leaderboard table

--- pairingGame/php/leaderboard.php
+++ pairingGame/php/leaderboard.php
@@ -12,7 +12,22 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
         <?php include("navbar.php"); ?>
         <div id="main">
-            <p>LEADERBOARD PAGE</p>
+            <div id="leaderboard">
+                <table>
+                    <tr>
+                        <th>Username</th>
+                        <th>Highscore</th>
+                    </tr>
+                    <tr>
+                        <td>name</td>
+                        <td>score</td>
+                    </tr>
+                    <tr>
+                        <td>name2</td>
+                        <td>score2</td>
+                    </tr>
+                </table>
+            </div>
         </div>
     </body>
 </html>
