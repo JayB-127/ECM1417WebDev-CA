52d063edf6c6a895bb300c0c8523426e2d84b9da
Jacob <jw.braidley@gmail.com>
2023-04-18 16:17:25 +0100

    prevents double newline character

--- pairingGame/php/pairs.php
+++ pairingGame/php/pairs.php
@@ -157,6 +157,7 @@
                 foreach ($lines as $line) {
                     $name = explode(", ", $line)[1];
                     if ($name == $username) {
+                        $msg = $score . ", " . $username;
                         $updatedContent = str_replace($line, $msg, $content);
                         file_put_contents("../data/leaderboard.csv", $updatedContent);
                         $containsName = true;
